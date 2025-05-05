a2248ac5d8e10552b10fbcbd6e2d2152f0d04b84
cinta <riadisafina@gmail.com>
2025-05-05 15:56:51 +0700

    login

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,43 +6,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use App\Models\User;
 
-class User extends Authenticatable
-{
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
-    {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
-    }
-}
+User::create([
+    'name' => 'Admin',
+    'email' => 'admin@example.com',
+    'password' => 'admin123' // <-- tidak perlu bcrypt(), Laravel akan meng-hash otomatis
+]);
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+//use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// login routes
+Route::get('/login',[AuthController::class, 'showLoginForm'])->name('login');
+Route::post('/login',[AuthController::class,'login']);
+
+// Setelah login berhasil
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware('auth');
+
+route::post('/logout',[AuthController::class, 'logout'])->name('logout');
